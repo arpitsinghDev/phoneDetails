3b3ee19a2acb428e96b6f06c757e8fcec80aa7d8
arpit singh <45120042+arpitsinghDev@users.noreply.github.com>
2019-12-11 12:28:04 +0530

    Update main.js

--- main.js
+++ main.js
@@ -28,7 +28,7 @@ success: function (response) {
     else{
         $('.optional').append("<div class='opcont"+i+"'></div>")
         $('.optional>.opcont'+i).append("<a target='_blank' href="+response[i]['producturl']+"><img src="+response[i]['img']+"></a>");
-        $('.optional>.opcont'+i).append("<div class='details'><h3 id='name'>"+response[i]['name']+"</h3><span>"+response[i]['flipkardprice']['amount']+" "+response[i]['flipkardprice']['currency']+"</span> <span> " +response[i]['discount']+ "%off</span><br><a target='_blank' href="+response[i]['producturl']+" class='buynow'>BUY NOW</a></div>")
+        $('.optional>.opcont'+i).append("<div class='details'><h3 id='name'>"+response[i]['name']+"</h3><span>"+response[i]['flipkardprice']['amount']+" "+response[i]['flipkardprice']['currency']+"</span> <span> " +response[i]['discount']+ "%off</span><br><span class='offer'>Flipkard "+response[j]['m_specialprice']['amount']+"/- "+response[j]['m_flipkardprice']['currency']+"<a target='_blank' href="+response[i]['producturl']+" class='buynow'>BUY NOW</a></div>")
     }
     }
     if($('.imgrate .'+b).html()==''){
